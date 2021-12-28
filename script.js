f809e54907cbfbaeea557ed10c950a52ccfd3295
Cleyton Alves <cleyton.alves.a@gmail.com>
2021-12-28 13:04:11 -0300

    Item 10 - add function clearAll

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let listToDo = document.getElementById('lista-tarefas');
 let textToDo = document.getElementById('texto-tarefa');
 let buttonSelector = document.getElementById('criar-tarefa');
+let buttonClear = document.getElementById('apaga-tudo');
+let buttonRemove = document.getElementById('remover-finalizados');
+
 
 //Button add the text in the list
 let list = '';
@@ -15,7 +18,7 @@ buttonSelector.addEventListener('click', buttonAdd);
 let gray = 'rgb(128, 128, 128)';
 function selectedItem(e){
     let itemChildren = listToDo.children
-    for(let i = 0;i<itemChildren.length; i +=1){
+    for(let i = 0;i < itemChildren.length; i += 1){
         if(itemChildren[i] === e.target){
             itemChildren[i].style.backgroundColor = gray;
         }else{
@@ -25,12 +28,22 @@ function selectedItem(e){
 }
 listToDo.addEventListener('click', selectedItem);
 //Add class completed with double click
-let listChildren = listToDo.children
+let listChildren = listToDo.children;
 function doubleClick(e){
-    for(let i = 0;i< listChildren.length; i += 1){
+    for(let i = 0;i < listChildren.length; i += 1){
         if(listChildren[i] === e.target){
             listChildren[i].classList.toggle('completed');
         }
     }
 }
 listToDo.addEventListener('dblclick', doubleClick);
+//Clear all to do list
+function clearAll(){
+    listToDo.remove();
+}
+buttonClear.addEventListener('click', clearAll);
+//Remove item of the list
+function removeItem(){
+        listToDo.lastElementChild.remove();
+}
+buttonRemove.addEventListener('click', removeItem);
